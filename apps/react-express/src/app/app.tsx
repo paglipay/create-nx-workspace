b24b1ef6217331f97183a75a6572934d3ccc914f
paglipay <paglipay@gmail.com>
2022-05-24 13:04:15 -0700

    heroku

--- apps/react-express/src/app/app.tsx
+++ apps/react-express/src/app/app.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Message } from '@create-nx-workspace/api-interfaces';
-
+import ProductPurchaseCard from './product-purchase-card/product-purchase-card';
 export const App = () => {
   const [m, setMessage] = useState<Message>({ message: '' });
 
@@ -13,7 +13,8 @@ export const App = () => {
   return (
     <>
       <div style={{ textAlign: 'center' }}>
-        <h1>Welcome to react-express!</h1>
+        <h1>Welcome react-express</h1>
+        <ProductPurchaseCard/>
         <img
           width="450"
           src="https://raw.githubusercontent.com/nrwl/nx/master/images/nx-logo.png"
